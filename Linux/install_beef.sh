905edb196b3f1eb3084249d6e0b54b38f1e5c2a0
Alex <83731549+asutherlin@users.noreply.github.com>
2021-09-19 22:18:19 -0500

    Linux update

---
+++ Linux/install_beef.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install -y ruby-full ruby ruby-dev
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+sudo apt -y update
+
+sudo apt -y install gcc-9
---
+++ Linux/lynis.partial.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo lynis audit --test-from-group malware,authentication,networking,storage,filesystems >> /tmp/lynis.partial_scan.log
